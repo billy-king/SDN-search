1dd739866e3541b70cdb8a4239912c08fb52e58f
Billyking <billyking19920205@gmail.com>
2015-08-18 15:30:58 +0800

    Webui

--- websocket/app.js
+++ websocket/app.js
@@ -1,26 +1,25 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express       = require('express');
+var path          = require('path');
+var app           = express();
+
+
+require('./middleware')(app);
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+/*
+ *static file設定 in project_path/public and project_path/bower_components
+ */
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(__dirname + '/bower_components'));
 
 app.use('/', routes);
 app.use('/users', users);
---
+++ websocket/middleware/index.js
@@ -0,0 +1,14 @@
+var favicon       = require('serve-favicon');
+var logger        = require('morgan');
+var cookieParser  = require('cookie-parser');
+var bodyParser    = require('body-parser');
+
+
+module.exports = function(app) {
+  // uncomment after placing your favicon in /public
+  //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+}
